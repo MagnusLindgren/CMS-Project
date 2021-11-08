ae690e8b5b913eef2d3fd7e5aa6d131ca7eb02cf
Magnus Lindgren <magnus.c.lindgren@gmail.com>
2021-11-08 14:51:17 +0100

    Fixed problems with archive page not showing correctly

--- functions.php
+++ functions.php
@@ -22,3 +22,43 @@ function register_navwalker(){
 	require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
 }
 add_action( 'after_setup_theme', 'register_navwalker' );
+
+function cptui_register_my_cpts_projects() {
+
+	/**
+	 * Post Type: Projects.
+	 */
+
+	$labels = [
+		"name" => __( "Projects", "custom-post-type-ui" ),
+		"singular_name" => __( "Project", "custom-post-type-ui" ),
+	];
+
+	$args = [
+		"label" => __( "Projects", "custom-post-type-ui" ),
+		"labels" => $labels,
+		"description" => "",
+		"public" => true,
+		"publicly_queryable" => true,
+		"show_ui" => true,
+		"show_in_rest" => true,
+		"rest_base" => "",
+		"rest_controller_class" => "WP_REST_Posts_Controller",
+		"has_archive" => true,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"delete_with_user" => false,
+		"exclude_from_search" => false,
+		"capability_type" => "post",
+		"map_meta_cap" => true,
+		"hierarchical" => false,
+		"rewrite" => [ "slug" => "projects", "with_front" => true ],
+		"query_var" => true,
+		"supports" => [ "title", "editor", "thumbnail" ],
+		"show_in_graphql" => false,
+	];
+
+	register_post_type( "projects", $args );
+}
+
+add_action( 'init', 'cptui_register_my_cpts_projects' );
--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 get_header(); ?>
 
 <h1><?php bloginfo('name'); ?></h1>
-
+<?php echo "index.php"?>
 <?php
 get_footer();
