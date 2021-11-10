e2e7449da63ba5a4a4ea9ffef882fea41c1a3788
Magnus Lindgren <magnus.c.lindgren@gmail.com>
2021-11-10 11:57:33 +0100

    about is about done

--- page-about.php
+++ page-about.php
@@ -1,13 +1,23 @@
 <?php
 get_header(); 
-
 $about_title = get_field('about_title');
+$about_desc = get_field('about_description');
+$about_imageArray = get_field('about_image');
+$about_image = $about_imageArray['sizes']['large'];
 ?>
-
-
-	<h1><?php echo $about_title ?></h1>
-	<?php get_template_part('/includes/section','content'); ?>
-
+<div class="col m-3">
+	<div class="row m-3">
+		<img src="<?php echo $about_image; ?>" class="img-thumbnail rounded mx-auto d-block">
+	</div>
+	<div class="row">
+		<div class="container bg-light rounded">
+		<h2 class="text-center"><?php echo $about_title; ?></h2>
+			<div class="">
+				<?php echo $about_desc; ?>
+			</div>
+		</div>	
+	</div>
+</div>
 
 
 <?php
